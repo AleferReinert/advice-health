8c921627d0068ca7d4654f40ee611973fa55d328
Alefer Reinert <aleferreinert@gmail.com>
2024-12-17 21:31:49 -0300

    ref: Field and NewAppointment components

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,24 +1,28 @@
-import { ReactNode } from 'react'
+import { ComponentProps } from 'react'
 import InputMask from 'react-input-mask'
 
-interface FieldProps {
+interface FieldProps extends ComponentProps<'input'> {
 	label: string
-	children: ReactNode
 	mask?: string
-	notRequired?: boolean
 }
 
-export function Field({ label, notRequired, children, mask }: FieldProps) {
+export function Field({ label, required, mask, inputMode, type, className, maxLength }: FieldProps) {
+	function CustomInput() {
+		return mask ? (
+			<InputMask mask={mask} type={type} alwaysShowMask inputMode={inputMode} maxLength={maxLength} />
+		) : (
+			<input className={className} type={type} inputMode={inputMode} required={required} maxLength={maxLength} />
+		)
+	}
+
 	return (
 		<div className='w-full'>
 			<div className='mb-4'>
 				<label className='[&_input]:border [&_input]:border-gray-300 [&_input]:px-4 [&_input]:py-2 [&_input]:w-full focus-within:border-primary transition'>
 					<div className='font-medium mb-1 text-sm'>
-						{label}:{!notRequired && <span className='text-red-600'> *</span>}
+						{label}:{required && <span className='text-red-600'> *</span>}
 					</div>
-					<InputMask mask={mask} alwaysShowMask>
-						{children}
-					</InputMask>
+					<CustomInput />
 				</label>
 			</div>
 		</div>
--- src/components/NewAppointment/NewAppointment.tsx
+++ src/components/NewAppointment/NewAppointment.tsx
@@ -34,26 +34,16 @@ export function NewAppointment() {
 		<form action='' className='flex flex-col gap-4'>
 			<fieldset className={fieldsetStyles}>
 				<legend className={legendStyles}>Paciente</legend>
-				<Field label='Nome completo'>
-					<input type='text' required />
-				</Field>
+				<Field label='Nome completo' required />
 
 				<div className='md:flex gap-4'>
-					<Field label='CPF' mask='999.999.999-99'>
-						<input type='text' inputMode='numeric' required />
-					</Field>
-
-					<Field label='Data de nascimento'>
-						<input type='date' required className='bg-transparent' />
-					</Field>
+					<Field label='CPF' mask='999.999.999-99' inputMode='numeric' required />
+					<Field label='Data de nascimento' type='date' className='bg-transparent' required />
 				</div>
+
 				<div className='md:grid grid-cols-[2fr_1fr] gap-4'>
-					<Field label='E-mail'>
-						<input type='email' required />
-					</Field>
-					<Field label='Telefone' mask='(99) 99999-9999'>
-						<input type='text' inputMode='numeric' required />
-					</Field>
+					<Field label='E-mail' type='email' required />
+					<Field label='Telefone' mask='(99) 99999-9999' inputMode='numeric' required />
 				</div>
 			</fieldset>
 
@@ -61,50 +51,29 @@ export function NewAppointment() {
 				<legend className={legendStyles}>Endereço</legend>
 
 				<div className='md:grid grid-cols-[1fr_3fr] gap-4'>
-					<Field label='CEP' mask='99999-999'>
-						<input type='text' inputMode='numeric' required />
-					</Field>
-
-					<Field label='Endereço'>
-						<input type='text' required />
-					</Field>
+					<Field label='CEP' mask='99999-999' inputMode='numeric' required />
+					<Field label='Endereço' required />
 				</div>
 
 				<div className='grid grid-cols-[1fr_3fr] gap-4'>
-					<Field label='Número'>
-						<input type='text' required inputMode='numeric' />
-					</Field>
-
-					<Field label='Complemento' notRequired>
-						<input type='text' required />
-					</Field>
+					<Field label='Número' inputMode='numeric' required />
+					<Field label='Complemento' />
 				</div>
 
 				<div className='md:grid grid-cols-[3fr_3fr_1fr] gap-4'>
-					<Field label='Bairro'>
-						<input type='text' required />
-					</Field>
-					<Field label='Cidade'>
-						<input type='text' required />
-					</Field>
-					<Field label='Estado'>
-						<input type='text' maxLength={2} required />
-					</Field>
+					<Field label='Bairro' required />
+					<Field label='Cidade' required />
+					<Field label='Estado' required maxLength={2} />
 				</div>
 			</fieldset>
 
 			<fieldset className={fieldsetStyles}>
 				<legend className={legendStyles}>Contato Emergencial</legend>
-				<Field label='Nome' notRequired>
-					<input type='text' />
-				</Field>
+
+				<Field label='Nome' />
 				<div className='grid grid-cols-2 gap-4'>
-					<Field label='Telefone' notRequired>
-						<input type='text' inputMode='numeric' />
-					</Field>
-					<Field label='Relação' notRequired>
-						<input type='text' />
-					</Field>
+					<Field label='Telefone' mask='(99) 99999-9999' inputMode='numeric' />
+					<Field label='Relação' />
 				</div>
 			</fieldset>
 
